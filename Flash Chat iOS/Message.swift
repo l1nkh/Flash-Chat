25e7214e66bd75b4acfd60adcd8720c63d661654
l1nkh <diogo.m.henriques98@gmail.com>
2026-01-25 01:04:57 +0000

    Implement and commit table views

---
+++ Flash Chat iOS/Message.swift
@@ -0,0 +1,9 @@
+//
+//  Message.swift
+//  Flash Chat iOS
+//
+//  Created by Diogo Henriques on 24/01/2026.
+//  Copyright © 2026 diogo-h. All rights reserved.
+//
+
+import Foundation
---
+++ Flash Chat iOS/Views/MessageCell.swift
@@ -0,0 +1,24 @@
+//
+//  MessageCell.swift
+//  Flash Chat iOS
+//
+//  Created by Diogo Henriques on 24/01/2026.
+//  Copyright © 2026 diogo-h. All rights reserved.
+//
+
+import UIKit
+
+class MessageCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
